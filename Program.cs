a4933d6a9918633ab463705b428a36c063c060d5
KristiBu <106319724+KristiBu888@users.noreply.github.com>
2023-09-03 19:47:35 +0300

    add hw_8_ready

---
+++ Program.cs
@@ -0,0 +1,161 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+//Попытка самой решить, напридумывала всякого, но не получилось.
+
+
+
+// Console.WriteLine("Первую размерность массива");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Вторую размерность массива");
+// int coloms = Convert.ToInt32(Console.ReadLine());
+// int maxNum = matrix[0, j];
+// int[,] matrix = new int[rows, coloms];
+
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         matrix[i, j] = new Random().Next(0, 10);
+//         Console.Write(matrix[i, j] + " ");
+//     }
+//     Console.WriteLine();
+// }
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+//     if (matrix[1, j] > maxNum)
+//     {
+//         maxNum = matrix[1, j];
+//         i++;
+//     }
+//     int changer = matrix.GetLength(0) - 1;
+//     int temp = matrix[0, j];
+//     matrix[0, j] = matrix[changer, j];
+//     matrix[changer, j] = temp;
+// }
+// Console.WriteLine();
+// for (int i = 0; i < matrix.GetLength(0); i++)
+// {
+//     for (int j = 0; j < matrix.GetLength(1); j++)
+//     {
+//         Console.Write(matrix[i, j] + " ");
+//     }
+//     Console.WriteLine();
+// } /что то я намудрила((
+
+int[,] matrix = new int[3, 4];
+CreateArrayRandom(matrix);
+PrintArray(matrix);
+SortToLower(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+void CreateArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void SortToLower(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+5 2 6 7
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
+int[,] matrix = new int[3, 4];
+CreateArrayRandom(matrix);
+PrintArray(matrix);
+the sum of the elements in each row(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
+void CreateArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void SortToLower(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1) - 1; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
